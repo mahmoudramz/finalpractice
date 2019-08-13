a49b451522140826d2fb17b53dfd6ede54e2f38e
mahmoudramz <52760874+mahmoudramz@users.noreply.github.com>
2019-08-13 14:33:43 -0400

    Add files via upload

---
+++ assets/data/productdata.ts
@@ -0,0 +1,16 @@
+import {Product} from '../../app/product';
+
+export const PRODUCTDATA : Product[] =[
+    {   id : ' 123',
+        desc: ' asd',
+        qunty : 4
+    },
+    {   id : ' 1234645',
+    desc: ' asd',
+    qunty : 5
+},
+{   id : ' 1235567',
+desc: ' asddfgdg',
+qunty : 45
+},
+]
---
+++ assets/data/profiledata.ts
@@ -0,0 +1,4 @@
+import {Profile} from '../../app/profile';
+
+export const PROFILEDATA : Profile = 
+    {pid : 'qwer', name :'COMPUTER', grad_year : 2029};
